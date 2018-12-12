b5bfe4da6d2d84e960cd437fada9b97f40faaf50
cfern052 <cfern052@gmail.com>
2018-12-12 15:29:23 -0500

    added text to java and director images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,26 @@
 
 var directors = ["Kubrick","Speilberg","Gilliam","PT Anderson"]
 var hints = ["2001 Space Odessy","Jaws","12 Monkeys","There Will Be Blood"]
+var letterGuess =[]
 
-var wins= 0;
-var losses= 0;
+var winsCount= 0;
+var lossCount= 0;
 var guessLeft= 12;
 
 
+var dirList = document.getElementById("directors")
+var hint = document.getElementById("hints")
+var winsText = document.getElementById("wins")
+var guess = document.getElementById("guessLeft")
+var lossCountText = document.getElementById("losses")
+var letterGuessText = document.getElementById("lettersGuessed")
 
+dirList.textContent =
+hint.textContent = 
+winsText.textContent = "wins:" + winCount;
+guess.textContent = "Letters guessed:" + letterGuess;
+lossCountText.textContent = "Losses:" + lossCount;
+letterGuessText.textContent = "Letters guessed:" + letterGuess;
 
 hints.onclick = function(){
 
@@ -15,11 +28,16 @@ hints.onclick = function(){
 
 document.onkeyup = function(event) {
 
-    var userGuess = event.key;
+    var guess = event.key;
     var compDirector = directors[Math.floor(Math.random() * directors.length)];
+
+};
+
+document.onkeyup = function(event) {
+
 
 }
 
-winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
-        tiesText.textContent = "Guesses Left: " + ties;
+winsText.textContent = "wins: " + wins+1;
+lossesText.textContent = "losses: " + losses+1;
+tiesText.textContent = "Guesses Left: " + ties;
